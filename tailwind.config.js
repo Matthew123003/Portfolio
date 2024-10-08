a7097b8c7d001fe2b9b7cb294291cb1546496863
matthew123003 <matthew123003@gmail.com>
2024-10-08 18:53:38 -0400

    Updated Tailwind CSS Config, Layout.jsx, added Header.jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,24 @@ module.exports = {
       padding: "15px",
     },
     screens: {
-        
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetBrainsMono)",
     },
     extend: {
+
+      colors: {
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        }
+      },
+
       keyframes: {
         "accordion-down": {
           from: { height: 0 },
