1d13bc29d9ed26662075090e6b6b9bf25cb77bb3
matthew123003 <matthew123003@gmail.com>
2024-10-18 09:37:06 -0400

    Next.js, Framer Motion, React-Icons, React.js, all updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
+    './components/**/*.{ts,tsx,js,jsx}',
     './app/**/*.{js,jsx}',
     './src/**/*.{js,jsx}',
 	],
